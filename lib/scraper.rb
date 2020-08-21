1986699546a0fdd9312c31f59bd508671fde542c
Philip Warren <philwarren1@gmail.com>
2020-08-21 16:09:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,8 @@ doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
 p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
 
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
  
 courses.each do |course|
   puts course.text.strip
